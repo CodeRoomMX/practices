4802515caf4b29063349ee0983361fb220bba8df
Wenceslao Negrete <vampay@digitaldealers.mx>
2018-03-12 20:04:08 -0600

    crear curso con array y split de string

--- 07_objects/06_crear_curso_array.js
+++ 07_objects/06_crear_curso_array.js
@@ -6,4 +6,25 @@ var cursos = [
   [ 'Algorithms', '301', 'Jessica', '11am-12pm' ],
 ];
 
+function crearCurso(horario, nombre){
+  var curso = {
+    horario: horario,
+    nombre: nombre,
+    profesor: '',
+    salon: '',
+    asignarProfesor: function(nombre){
+      this.profesor = nombre;
+    },
+    asignarSalon: function(salon){
+      this.salon = salon;
+    }
+  }
+  return curso;
+}
 
+for(var i = 0; i < cursos.length; i++){
+  var curso = crearCurso(cursos[i][3], cursos[i][0]);
+  curso.asignarProfesor(cursos[i][2]);
+  curso.asignarSalon(cursos[i][1]);
+  console.log(curso);
+}
--- 07_objects/07_cursos_parse_string.js
+++ 07_objects/07_cursos_parse_string.js
@@ -1,2 +1,31 @@
 var rawData = 'HTML|101|Miriam|8am-10am||CSS|102|Adrian|9am-10am||JS|103|Joy|10am-11am||NodeJS|201|Edgar|10am-11am||Algorithms|301|Jessica|11am-12pm';
 
+var rawCursos = rawData.split('||');
+var cursos = [];
+
+for(var i = 0; i < rawCursos.length; i++){
+  cursos[i] = rawCursos[i].split('|');
+}
+
+function crearCurso(horario, nombre){
+  var curso = {
+    horario: horario,
+    nombre: nombre,
+    profesor: '',
+    salon: '',
+    asignarProfesor: function(nombre){
+      this.profesor = nombre;
+    },
+    asignarSalon: function(salon){
+      this.salon = salon;
+    }
+  }
+  return curso;
+}
+
+for(var i = 0; i < cursos.length; i++){
+  var curso = crearCurso(cursos[i][3], cursos[i][0]);
+  curso.asignarProfesor(cursos[i][2]);
+  curso.asignarSalon(cursos[i][1]);
+  console.log(curso);
+}
